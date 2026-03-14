82d706450328e80b5f7dd72082e5ee0915489afe
Vaijayanthi-R <2k22ece104@kiot.ac.in>
2026-03-14 16:28:05 +0530

    added url be

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://hospital-management-uhvo.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
