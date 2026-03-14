61f7a51fe471b44b86fb57515562122781d3e774
Vaijayanthi-R <2k22ece104@kiot.ac.in>
2026-03-14 16:41:40 +0530

    FE AND BE

--- backend/src/main/java/com/hospital/config/SecurityConfig.java
+++ backend/src/main/java/com/hospital/config/SecurityConfig.java
@@ -58,7 +58,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of(
+          "http://localhost:3000",
+         "https://hospital-management-nkpq.vercel.app/"
+));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
